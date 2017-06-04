0d40529453cb8b54379a36fafb3706a7c6c6602b
Tanmay Bhagwat <tanmay.d.bhagwat@gmail.com>
2017-06-04 10:31:34 -0700

    Second commit

--- src/Hello.java
+++ src/Hello.java
@@ -4,5 +4,13 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello Tanmay!");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber+mySecondNumber+myThirdNumber;
+
+        System.out.println(100 - myTotal);
     }
 }
